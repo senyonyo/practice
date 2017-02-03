34b4c8142120584a768f91e81811c3f6315ff75f
柴田彩香 <shibataayaka@net44-dhcp43.sfc.keio.ac.jp>
2017-02-03 15:31:32 +0900

    逆表示

--- my-reverse.c
+++ my-reverse.c
@@ -6,15 +6,27 @@ char str[20];
 char rev[20];
 
 int action(){
-    len = strlen(str);
-    printf("\"%s\" の長さは %dです。\n", str, len);
+  len = strlen(str);
+  printf("\"%s\" の長さは %dです。\n", str, len);
+  printf("逆から表示すると、");
+  
+  int i,count;
+	for (i = 0; str[i] != '\0'; i++){
+		count++;
+	}
+
+	for (i=count - 1; i>= 0; i--){
+		printf("%c", str[i]);
+	}
+  
+  printf("です。");
+  return 0;
 }
 
 int main(){
   printf("文字列を入力してください。");
 	scanf("%s",str);
-	printf("入力された文字列は%sです。",str[20]);
 	action();
-	printf("逆から表示すると%sです。",rev[0]);
 	return 0;
 }
+
