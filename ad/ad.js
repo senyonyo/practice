a304ca3ffa3fe06305e2abb6af9cd418eb139617
Your Name <you@example.com>
2018-02-02 09:56:14 +0900

    fix view

--- ad/ad.js
+++ ad/ad.js
@@ -3,20 +3,20 @@ var kind =[ 'bitcoin','love','car'];
 function start(){
   i=1
   while (i<4){
-    rand.push(Math.floor(Math.random()*(3-1)+1));
+    rand.push(Math.floor(Math.random()*3+1));
     i++;
   }
-  alert(rand);
+  console.log(rand);
 
   //document.getElementById('2').innerHTML = "<img src=" + kind[0] + 1 + "-00" + 9 +".jpg>";
  
   for(j=1;j<4;j++){
     word = kind[j-1];
     for(k=1;k<10;k++){
-      document.getElementById(j).innerHTML += "<img src=\""+ word + j + "/" + word + j + "-00" + k +".jpg>";
-     /* if(k== 3 || 6){
-        document.getElementById(j).innerHTML += </br>;
-      }*/
+      document.getElementById(j).innerHTML += "<img src=\""+ word + rand[j-1] + "/" + word + rand[j-1] + "-00" + k +".jpg\">";
+     if(k == 3 || k == 6){
+        document.getElementById(j).innerHTML += "<br>";
+      }
     }
   }
   
