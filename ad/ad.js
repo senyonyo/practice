2bd87d11beab95b591f33b646d3eaa07cf54ffca
柴田彩香 <shibataayaka@MacBook-Air-Ayaka-Shibata.local>
2018-02-02 09:37:47 +0900

    ad.js変更

--- ad/ad.js
+++ ad/ad.js
@@ -7,20 +7,18 @@ function start(){
     i++;
   }
   alert(rand);
-  alert(kind[1]);
 
-  document.getElementById('2').innerHTML = "<img src=\"bitcoin1/bitcoin1-001.jpg\">";
-  //" <img src=" "+ kind[0] + 1 + "-00" + 1 +".jpg";
+  //document.getElementById('2').innerHTML = "<img src=" + kind[0] + 1 + "-00" + 9 +".jpg>";
  
-
-/*  for(j=1;j<4;j++){
+  for(j=1;j<4;j++){
     word = kind[j-1];
     for(k=1;k<10;k++){
-      document.getElementById(j).innerHTML = " <img src=" "+ word + j + "-00" + k +".jpg"
-      if(k== 3 || 6){
-        document.getElementById(j).innerHTML += </br>
-      }
-    }*/
-  
+      rand.push(k);
+      document.getElementById(j).innerHTML += "<img src=\""+ word + j + "/" + word + j + "-00" + k +".jpg>";
+     /* if(k== 3 || 6){
+        document.getElementById(j).innerHTML += </br>;
+      }*/
+    }
+  }
   
 };
